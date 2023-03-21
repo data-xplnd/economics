d71aa48b6a47ab019854951555ba8f1b93de6a33
ryann-green <ryanngreen2@gmail.com>
2023-03-20 22:13:19 -0500

    updated with pce data pull

---
+++ pce.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import requests
+import os
+from sqlalchemy import create_engine
+from urllib.parse import quote
+from io import StringIO
+from creds import pw
+from creds import db
+
+# function to add table to db
+def add_table(df,table):
+
+    # script to create sql database for needed tables
+    engine = create_engine(f'mysql+pymysql://admin:{pw}@{db}')
+    df.to_sql(f"{table}",con=engine, if_exists='replace')
+
+# url and content for BEA PCE data
+# the csv comes from and is to be updated monthly:
+# https://apps.bea.gov/iTable/?reqid=19&step=2&isuri=1&categories=survey#eyJhcHBpZCI6MTksInN0ZXBzIjpbMSwyLDNdLCJkYXRhIjpbWyJjYXRlZ29yaWVzIiwiU3VydmV5Il0sWyJOSVBBX1RhYmxlX0xpc3QiLCI2NSJdXX0=
+df=pd.read_csv('economics/inputs/pce.csv')
+
+add_table(df,'pce')
+
+print('pce table added to db')
